f7cfe4565d0f7967e064b88a19c44845042aac90
cg33 <chg80333@163.com>
2020-01-07 20:06:57 +0800

    update echart

--- echarts/chart.go
+++ echarts/chart.go
@@ -3,10 +3,12 @@ package echarts
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/go-echarts/go-echarts/charts"
 	"github.com/go-echarts/go-echarts/datasets"
 	"github.com/go-echarts/go-echarts/templates"
 	"html/template"
+	"regexp"
 	"strings"
 )
 
@@ -55,107 +57,7 @@ func (c *Chart) GetName() string {
 
 func (c *Chart) GetContent() template.HTML {
 
-	buf := new(bytes.Buffer)
-
-	if line, ok := c.Content.(*charts.Line); ok {
-		_ = line.Render(buf)
-	}
-
-	if bar, ok := c.Content.(*charts.Bar); ok {
-		_ = bar.Render(buf)
-	}
-
-	if bar3d, ok := c.Content.(*charts.Bar3D); ok {
-		_ = bar3d.Render(buf)
-	}
-
-	if box, ok := c.Content.(*charts.BoxPlot); ok {
-		_ = box.Render(buf)
-	}
-
-	if chart3d, ok := c.Content.(*charts.Chart3D); ok {
-		_ = chart3d.Render(buf)
-	}
-
-	if rc, ok := c.Content.(*charts.RectChart); ok {
-		_ = rc.Render(buf)
-	}
-
-	if ef, ok := c.Content.(*charts.EffectScatter); ok {
-		_ = ef.Render(buf)
-	}
-
-	if fu, ok := c.Content.(*charts.Funnel); ok {
-		_ = fu.Render(buf)
-	}
-
-	if ga, ok := c.Content.(*charts.Gauge); ok {
-		_ = ga.Render(buf)
-	}
-
-	if geo, ok := c.Content.(*charts.Geo); ok {
-		_ = geo.Render(buf)
-	}
-
-	if graph, ok := c.Content.(*charts.Graph); ok {
-		_ = graph.Render(buf)
-	}
-
-	if hm, ok := c.Content.(*charts.HeatMap); ok {
-		_ = hm.Render(buf)
-	}
-
-	if kl, ok := c.Content.(*charts.Kline); ok {
-		_ = kl.Render(buf)
-	}
-
-	if line3d, ok := c.Content.(*charts.Line3D); ok {
-		_ = line3d.Render(buf)
-	}
-
-	if li, ok := c.Content.(*charts.Liquid); ok {
-		_ = li.Render(buf)
-	}
-
-	if ma, ok := c.Content.(*charts.Map); ok {
-		_ = ma.Render(buf)
-	}
-
-	if pa, ok := c.Content.(*charts.Parallel); ok {
-		_ = pa.Render(buf)
-	}
-
-	if pi, ok := c.Content.(*charts.Pie); ok {
-		_ = pi.Render(buf)
-	}
-
-	if ra, ok := c.Content.(*charts.Radar); ok {
-		_ = ra.Render(buf)
-	}
-
-	if sk, ok := c.Content.(*charts.Sankey); ok {
-		_ = sk.Render(buf)
-	}
-
-	if sc, ok := c.Content.(*charts.Scatter); ok {
-		_ = sc.Render(buf)
-	}
-
-	if sc3d, ok := c.Content.(*charts.Scatter3D); ok {
-		_ = sc3d.Render(buf)
-	}
-
-	if su, ok := c.Content.(*charts.Surface3D); ok {
-		_ = su.Render(buf)
-	}
-
-	if tr, ok := c.Content.(*charts.ThemeRiver); ok {
-		_ = tr.Render(buf)
-	}
-
-	if wc, ok := c.Content.(*charts.WordCloud); ok {
-		_ = wc.Render(buf)
-	}
+	buf, chartId := c.getContent()
 
 	content := buf.String()
 
@@ -169,5 +71,154 @@ func (c *Chart) GetContent() template.HTML {
 	content = strings.Replace(content, `<div class="select" style="margin-right:10px; margin-top:10px; position:fixed; right:10px;"></div>`, "", - 1)
 	content = strings.Replace(content, `container`, "echarts-container", - 1)
 
-	return template.HTML(content)
+	return template.HTML(content) + template.HTML(fmt.Sprintf(resizeJS, chartId))
 }
+
+func (c *Chart) GetOptions() template.JS {
+
+	buf, _ := c.getContent()
+
+	reg, _ := regexp.Compile(`{([\s\S]*)};`)
+
+	match := reg.FindString(buf.String())
+
+	return template.JS(match[:len(match)-1])
+}
+
+func (c *Chart) getContent() (*bytes.Buffer, string) {
+	buf := new(bytes.Buffer)
+	chartId := ""
+
+	if line, ok := c.Content.(*charts.Line); ok {
+		chartId = line.ChartID
+		_ = line.Render(buf)
+	}
+
+	if bar, ok := c.Content.(*charts.Bar); ok {
+		chartId = bar.ChartID
+		_ = bar.Render(buf)
+	}
+
+	if bar3d, ok := c.Content.(*charts.Bar3D); ok {
+		chartId = bar3d.ChartID
+		_ = bar3d.Render(buf)
+	}
+
+	if box, ok := c.Content.(*charts.BoxPlot); ok {
+		chartId = box.ChartID
+		_ = box.Render(buf)
+	}
+
+	if chart3d, ok := c.Content.(*charts.Chart3D); ok {
+		chartId = chart3d.ChartID
+		_ = chart3d.Render(buf)
+	}
+
+	if rc, ok := c.Content.(*charts.RectChart); ok {
+		chartId = rc.ChartID
+		_ = rc.Render(buf)
+	}
+
+	if ef, ok := c.Content.(*charts.EffectScatter); ok {
+		chartId = ef.ChartID
+		_ = ef.Render(buf)
+	}
+
+	if fu, ok := c.Content.(*charts.Funnel); ok {
+		chartId = fu.ChartID
+		_ = fu.Render(buf)
+	}
+
+	if ga, ok := c.Content.(*charts.Gauge); ok {
+		chartId = ga.ChartID
+		_ = ga.Render(buf)
+	}
+
+	if geo, ok := c.Content.(*charts.Geo); ok {
+		chartId = geo.ChartID
+		_ = geo.Render(buf)
+	}
+
+	if graph, ok := c.Content.(*charts.Graph); ok {
+		chartId = graph.ChartID
+		_ = graph.Render(buf)
+	}
+
+	if hm, ok := c.Content.(*charts.HeatMap); ok {
+		chartId = hm.ChartID
+		_ = hm.Render(buf)
+	}
+
+	if kl, ok := c.Content.(*charts.Kline); ok {
+		chartId = kl.ChartID
+		_ = kl.Render(buf)
+	}
+
+	if line3d, ok := c.Content.(*charts.Line3D); ok {
+		chartId = line3d.ChartID
+		_ = line3d.Render(buf)
+	}
+
+	if li, ok := c.Content.(*charts.Liquid); ok {
+		chartId = li.ChartID
+		_ = li.Render(buf)
+	}
+
+	if ma, ok := c.Content.(*charts.Map); ok {
+		chartId = ma.ChartID
+		_ = ma.Render(buf)
+	}
+
+	if pa, ok := c.Content.(*charts.Parallel); ok {
+		chartId = pa.ChartID
+		_ = pa.Render(buf)
+	}
+
+	if pi, ok := c.Content.(*charts.Pie); ok {
+		chartId = pi.ChartID
+		_ = pi.Render(buf)
+	}
+
+	if ra, ok := c.Content.(*charts.Radar); ok {
+		chartId = ra.ChartID
+		_ = ra.Render(buf)
+	}
+
+	if sk, ok := c.Content.(*charts.Sankey); ok {
+		chartId = sk.ChartID
+		_ = sk.Render(buf)
+	}
+
+	if sc, ok := c.Content.(*charts.Scatter); ok {
+		chartId = sc.ChartID
+		_ = sc.Render(buf)
+	}
+
+	if sc3d, ok := c.Content.(*charts.Scatter3D); ok {
+		chartId = sc3d.ChartID
+		_ = sc3d.Render(buf)
+	}
+
+	if su, ok := c.Content.(*charts.Surface3D); ok {
+		chartId = su.ChartID
+		_ = su.Render(buf)
+	}
+
+	if tr, ok := c.Content.(*charts.ThemeRiver); ok {
+		chartId = tr.ChartID
+		_ = tr.Render(buf)
+	}
+
+	if wc, ok := c.Content.(*charts.WordCloud); ok {
+		chartId = wc.ChartID
+		_ = wc.Render(buf)
+	}
+
+	return buf, chartId
+}
+
+const resizeJS = `<script>
+	window.addEventListener('resize',function () {
+		myChart_%s.resize();
+	})
+</script>`
