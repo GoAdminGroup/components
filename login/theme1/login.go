82b2078e693e467813ed59c18f6f2336baffc659
cg33 <chg80333@163.com>
2020-04-21 07:44:56 +0800

    update login theme

--- login/theme1/login.go
+++ login/theme1/login.go
@@ -3,18 +3,51 @@ package theme1
 import (
 	"bytes"
 	"github.com/GoAdminGroup/go-admin/modules/logger"
+	"github.com/GoAdminGroup/go-admin/modules/utils"
 	"github.com/GoAdminGroup/go-admin/template/login"
 	"html/template"
+	textTemplate "text/template"
 )
 
-type Login struct{}
+type Login struct {
+	TencentWaterProofWallID string
+	BackgroundColor         string
+	LoginBtnColor           string
+}
 
-func Get() *Login { return new(Login) }
+type Config struct {
+	TencentWaterProofWallID string
+	LoginBtnColor           string
+	BackgroundColor         string
+}
+
+func Get(cfg ...Config) *Login {
+	if len(cfg) > 0 {
+		return &Login{
+			TencentWaterProofWallID: cfg[0].TencentWaterProofWallID,
+			BackgroundColor:         utils.SetDefault(cfg[0].BackgroundColor, "", "#2d3a4b"),
+			LoginBtnColor:           utils.SetDefault(cfg[0].LoginBtnColor, "", "#6a83a2"),
+		}
+	}
+	return &Login{BackgroundColor: "#2d3a4b", LoginBtnColor: "#6a83a2"}
+}
 
 func (l *Login) GetTemplate() (*template.Template, string) {
+
+	t := textTemplate.New("login_theme1").Delims("{%", "%}")
+	t, err := t.Parse(List["login/theme1"])
+	if err != nil {
+		logger.Error("login component, get template parse error: ", err)
+	}
+	buf := new(bytes.Buffer)
+	err = t.Execute(buf, l)
+	if err != nil {
+		logger.Error("login component, get template execute error: ", err)
+	}
+
 	tmpl, err := template.New("login_theme1").
 		Funcs(login.DefaultFuncMap).
-		Parse(List["login/theme1"])
+		Parse(buf.String())
 
 	if err != nil {
 		logger.Error("login component, get template error: ", err)
