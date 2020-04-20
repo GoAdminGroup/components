bcdd7dd7d3900c3025145318c24513a6a9c57ec0
cg33 <chg80333@163.com>
2020-04-20 21:23:03 +0800

    update theme

---
+++ login/theme1/assets_list.go
@@ -0,0 +1,9 @@
+package theme1
+
+var AssetsList = []string{
+	"/login/dist/all.min.css",
+	"/login/dist/all.min.js",
+	"/login/dist/respond.min.js",
+	"/login/dist/fonts/fontawesome-webfont.woff2",
+	"/login/dist/fonts/glyphicons-halflings-regular.woff2",
+}
--- login/theme1/login.go
+++ login/theme1/login.go
@@ -1,31 +1,39 @@
 package theme1
 
 import (
-	"github.com/chenhg5/go-admin/modules/logger"
+	"bytes"
+	"github.com/GoAdminGroup/go-admin/modules/logger"
+	"github.com/GoAdminGroup/go-admin/template/login"
 	"html/template"
 )
 
-type Login struct {
-}
+type Login struct{}
 
-func Get() *Login {
-	return new(Login)
-}
+func Get() *Login { return new(Login) }
 
-func (*Login) GetTemplate() (*template.Template, string) {
-	tmpl, err := template.New("login_theme1").Parse(List["login/theme1"])
+func (l *Login) GetTemplate() (*template.Template, string) {
+	tmpl, err := template.New("login_theme1").
+		Funcs(login.DefaultFuncMap).
+		Parse(List["login/theme1"])
 
 	if err != nil {
-		logger.Error("Login GetTemplate Error: ", err)
+		logger.Error("login component, get template error: ", err)
 	}
 
 	return tmpl, "login_theme1"
 }
 
-func (*Login) GetAssetList() []string {
-	return asserts
-}
+func (l *Login) GetAssetList() []string               { return AssetsList }
+func (l *Login) GetAsset(name string) ([]byte, error) { return Asset(name[1:]) }
+func (l *Login) GetName() string                      { return "login" }
+func (l *Login) IsAPage() bool                        { return true }
 
-func (*Login) GetAsset(name string) ([]byte, error) {
-	return Asset(name[1:])
+func (l *Login) GetContent() template.HTML {
+	buffer := new(bytes.Buffer)
+	tmpl, defineName := l.GetTemplate()
+	err := tmpl.ExecuteTemplate(buffer, defineName, l)
+	if err != nil {
+		logger.Error("login component, compose html error:", err)
+	}
+	return template.HTML(buffer.String())
 }
